77ae6f4e18989378fdb5b2716ce4b1506c756af3
Ed Kiefer <ed.h.kiefer@gmail.com>
2017-06-29 16:59:27 -0400

    Cheap hack to turn on -configure --enable-tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,20 +1,9 @@
 #
-# Toy - fifth attempt at .travis-ci.sh 
+# Toy - Hack to get ocaml setup.ml -configure --enable-tests to work
 #
-OPAM_DEPENDS="ocamlfind ounit re"
-case "$OCAML_VERSION,$OPAM_VERSION" in
-4.02.1,1.2.2) ppa=avsm/ocaml42+opam12 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1;;
-esac
 
-echo "yes" | sudo add-apt-repository ppa:$ppa
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export MERLIN_TRAVIS=true
-opam init
-opam install ${OPAM_DEPENDS}
-eval `opam config env`
-make
-make test
+export CONFIGFLAGS=--enable-tests
+
 
